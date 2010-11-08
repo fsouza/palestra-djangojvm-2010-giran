9321b4349971ecec919d1547f92ee857163c5f55
Francisco Souza <francisco@franciscosouza.net>
2010-11-07 22:46:29 -0200

    Mudança no model Arquivo

--- lentoxer/core/models.py
+++ lentoxer/core/models.py
@@ -2,5 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Arquivo(models.Model):
-    nome = models.CharField(max_length=100)
     arquivo = models.FileField(upload_to="arquivos", max_length=100)
